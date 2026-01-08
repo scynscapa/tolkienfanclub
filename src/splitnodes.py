5d47303dab68e870cd6fde84e3006f4cdd1b74f9
scynscapa <607745+scynscapa@users.noreply.github.com>
2026-01-07 19:05:14 -0500

    Add split on images and links

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extractmarkdown import extract_markdown_images, extract_markdown_links
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -35,5 +36,39 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             result.append(TextNode(node_text[0], TextType.TEXT))
             result.append(TextNode(node_text[1], node_type))
             result.append(TextNode(node_text[2], TextType.TEXT))
+
+    return result
+
+def split_nodes_image(old_nodes):
+    result = list()
+
+    for node in old_nodes:
+        remaining_text = node.text
+        
+        for image_alt, image_url in extract_markdown_images(remaining_text):
+            sections = remaining_text.split(f"![{image_alt}]({image_url})", 1)
+            if sections[0] != "":
+                result.append(TextNode(sections[0], TextType.TEXT))
+                
+            result.append(TextNode(image_alt, TextType.IMAGE, image_url))
+            remaining_text = sections[1]
+
+    return result
+
+
+
+def split_nodes_link(old_nodes):
+    result = list()
+
+    for node in old_nodes:
+        remaining_text = node.text
+        
+        for link_text, link_url in extract_markdown_links(remaining_text):
+            sections = remaining_text.split(f"[{link_text}]({link_url})", 1)
+            if sections[0] != "":
+                result.append(TextNode(sections[0], TextType.TEXT))
+
+            result.append(TextNode(link_text, TextType.LINK, link_url))
+            remaining_text = sections[1]
 
     return result
--- src/test_split_nodes_delim.py
+++ src/test_split_nodes_delim.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from splitnodes import split_nodes_delimiter
+from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_split_eq(self):
@@ -31,6 +31,43 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             TextNode("end", TextType.ITALIC)
         ]
         self.assertEqual(new_nodes, result)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+            ],
+            new_nodes,
+        )
+            
+
 
 if __name__ == "__main__":
     unittest.main()
